aa9a99b9ccd019316c4f0f84c0e3fa1245a9b7fd
anhphan68 <159033620+anhphan68@users.noreply.github.com>
2024-02-05 20:54:15 +0700

    anchor program

--- programs/solana-nft-anchor/src/lib.rs
+++ programs/solana-nft-anchor/src/lib.rs
@@ -13,7 +13,7 @@ use anchor_spl::{
 };
 use mpl_token_metadata::accounts::{ MasterEdition, Metadata as MetadataAccount };
 
-declare_id!("<UPDATE HERE>");
+declare_id!("FvnFGphYBgZrJukjp5nfC8JsdgpNkBdrkksNFvXk1ZTo");
 #[program]
 pub mod solana_nft_anchor {
     use super::*;
